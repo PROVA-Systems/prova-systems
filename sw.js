252b207ee60232e6a14035ad7a1c90ab0e3e8d18
PROVA-Systems <admin@prova-systems.de>
2026-04-04 11:37:16 +0200

    404 Fehler 1.3

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
               Network-Only für APIs
 ============================================================ */
 
-const CACHE_VERSION = 'prova-v57';
+const CACHE_VERSION = 'prova-v22';
 const SYNC_TAG = 'prova-sync-queue';
 
 const APP_SHELL = [
